91aae9574f03993945d5f4a745af9256152a65e2
Fred LASNIER <fred@ouetquand.biz>
2021-04-18 16:56:03 +0200

    à mi course Ici c'est pas encore bien beau

--- App.svelte
+++ App.svelte
@@ -1,8 +1,25 @@
 <script>
   import DocumentForm from "./src/DocumentForm.svelte";
   let doc = {
-    amount: 12.34,
-    currency: "EUR"
+    doc: {
+      type: "Devis",
+      refence: "9999-999",
+      date: "JJ/MM/AAAA"
+    },
+    object: "Honoraires Mmm AAAA",
+    amount: 1.2,
+    currency: "EUR",
+    from: {
+      name: "my company",
+      mail: "mail",
+      phone: "phone",
+      address: "23 rue Émille Flombard\n75001 Paris\nFrance"
+    },
+    to: {
+      name: "to company name",
+      address: "10 rue de la poupée qui toiusse\n75004 Paris\nFrance",
+      siren: "999 999 999"
+    }
   };
 </script>
 
--- src/DocumentForm.svelte
+++ src/DocumentForm.svelte
@@ -1,35 +1,82 @@
 <script>
   export let document = {
-    typeDoc: 'Facture',
-    invoiceNb = '9999-999',
-    dateIssued = 'JJ/MM/AAAA',
-    object: 'Honoraires Mmm AAAA',
+    doc: {
+      type: "Devis",
+      refence: "9999-999",
+      date: "JJ/MM/AAAA"
+    },
+    dateIssued: "JJ/MM/AAAA",
+    object: "Honoraires Mmm AAAA",
     amount: 1.2,
-    currency: 'EUR',
+    currency: "EUR",
     from: {
-      name: 'my company',
-      mail: 'mail',
-      phone: 'phone',
-      address: '23 rue Émille Flombard\n75001 Paris\nFrance'
+      name: "my company",
+      mail: "mail",
+      phone: "phone",
+      address: "23 rue Émille Flombard\n75001 Paris\nFrance"
     },
     to: {
-      name: 'to company name',
-      address: '10 rue de la poupée qui toiusse\n75004 Paris\nFrance',
-      siren: '999 999 999'
+      name: "to company name",
+      address: "10 rue de la poupée qui toiusse\n75004 Paris\nFrance",
+      siren: "999 999 999"
     }
   };
 </script>
 
+<style>
+  legend {
+    background-color: rgb(60, 160, 95);
+  }
+  label::after {
+    display: block;
+    align: left;
+    content: "\A";
+  }
+</style>
+
 <form>
   <fieldset>
+    <legend>Général</legend>
+      <label>Date
+        <input type="text" bind:value={document.doc.date} />
+      </label>
+      <label>Type document
+        <input type="text" bind:value={document.doc.type} list="lstDocType"/>
+        <datalist id="lstDocType">
+          <option value="Facture" />
+          <option value="Pro-Format" />
+          <option value="Devis" />
+        </datalist>
+      </label>
+      <label>Référence
+        <input type="text" bind:value={document.doc.refence} />
+      </label>
+  </fieldset>
+  <fieldset>
+    <legend>Origine</legend>
+      <label>Nom
+        <input type="text" bind:value={document.from.name} />
+      </label>
+      <label>email
+        <input type="text" bind:value={document.from.mail} />
+      </label>
+  </fieldset>
+  <fieldset>
     <legend>Destinataire</legend>
-      <label>Nom<label>
-      <input type="text" bind:value={document.to.name} />
-      <input type="text" bind:value={document.to.siren} />
+      <label>Nom
+        <input type="text" bind:value={document.to.name} />
+      </label>
+      <label>SIREN
+        <input type="text" bind:value={document.to.siren} />
+      </label>
   </fieldset>
   <fieldset>
     <legend>Montant</legend>
-      <input type="number" step="0.01" bind:value={document.amount} />
-      <input type="text" maxlength="3" bind:value={document.currency} />
+      <label>Montant
+        <input type="number" step="0.01" bind:value={document.amount} />
+      </label>
+      <label>Devise
+        <input type="text" maxlength="3" bind:value={document.currency} />
+      </label>
   </fieldset>
 </form>
