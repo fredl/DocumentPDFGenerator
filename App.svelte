aedff8c81fee268231e727c118f0cd767637ed0d
Fred LASNIER <fred@ouetquand.biz>
2021-04-18 13:59:01 +0200

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import DocumentForm from "./src/DocumentForm.svelte";
+  let doc = {
+    amount: 12.34,
+    currency: "EUR"
+  };
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Document de test</h1>
+	<h2>Saisir les paramètres du document</h2>
+  <DocumentForm document={doc} />
+</main>
---
+++ src/DocumentForm.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let document = {
+    typeDoc: 'Facture',
+    invoiceNb = '9999-999',
+    dateIssued = 'JJ/MM/AAAA',
+    object: 'Honoraires Mmm AAAA',
+    amount: 1.2,
+    currency: 'EUR',
+    from: {
+      name: 'my company',
+      mail: 'mail',
+      phone: 'phone',
+      address: '23 rue Émille Flombard\n75001 Paris\nFrance'
+    },
+    to: {
+      name: 'to company name',
+      address: '10 rue de la poupée qui toiusse\n75004 Paris\nFrance',
+      siren: '999 999 999'
+    }
+  };
+</script>
+
+<form>
+  <fieldset>
+    <legend>Destinataire</legend>
+      <label>Nom<label>
+      <input type="text" bind:value={document.to.name} />
+      <input type="text" bind:value={document.to.siren} />
+  </fieldset>
+  <fieldset>
+    <legend>Montant</legend>
+      <input type="number" step="0.01" bind:value={document.amount} />
+      <input type="text" maxlength="3" bind:value={document.currency} />
+  </fieldset>
+</form>
